55e1040872260f436b5c4616ac4b68174b8ae089
Abdulah-alsmari <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-11-23 19:59:07 +0300

    working on viewModels

--- app/src/main/java/com/application/todolist/viewmodls/ToDoViewModel.kt
+++ app/src/main/java/com/application/todolist/viewmodls/ToDoViewModel.kt
@@ -10,35 +10,7 @@ private const val TAG = "ViewModelInit"
 
 class ToDoViewModel : ViewModel() {
 
-    private val responseLiveData: MutableLiveData<MutableList<ToDoModel>> = MutableLiveData()
 
-    companion object{
-        var responseUpdatedList = mutableListOf<ToDoModel>()
-    }
-    var title=MutableLiveData<String>()
-    var subtitle =MutableLiveData<String>()
-    var date =MutableLiveData<String>()
-    var note =MutableLiveData<String>()
-    init {
-        responseLiveData.value = createResponse()
-    }
-
-
-    fun getResponse(): LiveData<MutableList<ToDoModel>> {
-        return responseLiveData
-    }
-
-    fun createResponse(): MutableList<ToDoModel> {
-        return responseUpdatedList
-    }
-    fun diplay(postion:Int){
-        var t= responseUpdatedList[postion]
-        title.value=t.title
-        subtitle.value=t.subTitle
-        note.value=t.notes
-        date.value=t.date
-
-    }
 
 
 
